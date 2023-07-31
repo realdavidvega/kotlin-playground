dfb1cf3c9684113f9c25d1d1efb236feb7d9ce0f
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2023-07-31 21:22:56 +0200

    feat: add simple delegation example for teaching

---
+++ kotlin/code/_00_playground/src/main/kotlin/delegation/Operations.kt
@@ -0,0 +1,35 @@
+package delegation
+
+interface Operations {
+  fun sum(a: Int, b: Int): Int
+  fun multiply(a: Int, b: Int): Int
+}
+
+class OperationsBase : Operations {
+  override fun sum(a: Int, b: Int): Int = a + b
+  override fun multiply(a: Int, b: Int): Int = a * b
+}
+
+class OperationsComplex(private operations: OperationsBase) : Operations {
+  override fun sum(a: Int, b: Int): Int = operations.sum(a, b)
+  override fun multiply(a: Int, b: Int): Int = operations.multiply(a, b)
+  fun divide(a: Int, b: Int): Int = a / b
+}
+
+class OperationsDelegation(private val operations: Operations) : Operations by operations {
+    fun divide(a: Int, b: Int): Int = a / b
+}
+
+fun main() {
+    val operationsBase = OperationsBase()
+    val delegation = OperationsComplex(operationsBase)
+    println(delegation.sum(1, 2))
+    println(delegation.multiply(1, 2))
+    println(delegation.divide(1, 2))
+
+    // delegation
+    val operationsDelegation = OperationsDelegation(operationsBase)
+    println(operationsDelegation.sum(1, 2))
+    println(operationsDelegation.multiply(1, 2))
+    println(operationsDelegation.divide(1, 2))
+}
