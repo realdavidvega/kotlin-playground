11bef4d822f3a76fa07b05eeac620b1ed5f38eca
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2023-11-15 09:42:23 +0100

    feat: accumulating raised errors

--- kotlin/code/_00_playground/src/main/kotlin/func/Raises.kt
+++ kotlin/code/_00_playground/src/main/kotlin/func/Raises.kt
@@ -2,10 +2,16 @@
 
 package func
 
+import arrow.core.Either
+import arrow.core.NonEmptyList
+import arrow.core.mapOrAccumulate
 import arrow.core.raise.Raise
+import arrow.core.raise.RaiseAccumulate
 import arrow.core.raise.catch
 import arrow.core.raise.fold
+import arrow.core.raise.mapOrAccumulate
 import arrow.core.raise.recover
+import arrow.core.raise.withError
 
 // 4 - arrow's raise context
 
@@ -56,10 +62,16 @@ object Raises {
 
   data object NegativeSalary : JobError
 
+  // for later error accumulation inside a new type
+  data class JobErrors(val messages: String = "")
+
   // Raise context
   interface Jobs {
     context(Raise<JobError>)
     fun findById(jobId: JobId): Job
+
+    context(Raise<JobError>)
+    fun findAll(): List<Job>
   }
 
   // we can use the raise function from the DSL
@@ -67,6 +79,12 @@ object Raises {
   class LiveJobs : Jobs {
     context(Raise<JobError>)
     override fun findById(jobId: JobId): Job = JOBS_DATABASE[jobId] ?: raise(JobNotFound(jobId))
+
+    context(Raise<JobError>)
+    override fun findAll(): List<Job> =
+      catch({ JOBS_DATABASE.values.toList() }) { _: Throwable ->
+        raise(GenericError("An error occurred while retrieving all the jobs"))
+      }
   }
 
   // the alternative without context receivers would be something like this
@@ -81,7 +99,7 @@ object Raises {
   }
 
   // to create some raise context, we can use fold
-  class JobsService(private val jobs: Jobs) {
+  class JobsService(private val jobs: Jobs, private val converter: RaiseCurrencyConverter) {
     // creates a default raise context (DefaultRaise), and consumes it
     fun printSalaryFold(jobId: JobId): Unit =
       fold(
@@ -102,7 +120,87 @@ object Raises {
         // also won't intercept serious errors: VM, interrupted (threads), cancellation (coroutines)
         // catch = { throwable: Throwable -> println("A serious error occurred: $throwable") }
       )
+
+    // composing Raise<E> computations
+    // we are only interested in the happy path in this case
+    context(Raise<JobError>)
+    fun getSalaryGapWithMax(jobId: JobId): Double {
+      val job: Job = jobs.findById(jobId)
+      val jobList: List<Job> = jobs.findAll()
+      val maxSalary: Salary = jobList.maxSalary()
+      return maxSalary.value - job.salary.value
+    }
+
+    // extension function with raise using context receivers
+    context(Raise<JobError>)
+    private fun List<Job>.maxSalary(): Salary =
+      if (isEmpty()) {
+        raise(GenericError("No jobs found"))
+      } else {
+        this.maxBy { it.salary.value }.salary
+      }
+
+    // handling logic errors from different hierarchies in the same method
+    // won't compile without NegativeAmount raise context
+    context(Raise<JobError>, Raise<NegativeAmount>)
+    fun getSalaryGapWithMaxInEur(jobId: JobId): Double {
+      val job: Job = jobs.findById(jobId)
+      val jobList: List<Job> = jobs.findAll()
+      val maxSalary: Salary = jobList.maxSalary()
+      val salaryGap = maxSalary.value - job.salary.value
+      return converter.convertUsdToEurRaisingNegativeAmount(salaryGap)
+    }
+
+    // second option, using withError
+    // eventually the NegativeAmount will get converted to NegativeSalary error type and raised
+    // again
+    context(Raise<JobError>)
+    fun getSalaryGapWithMaxInEur(jobId: JobId): Double {
+      val job: Job = jobs.findById(jobId)
+      val jobList: List<Job> = jobs.findAll()
+      val maxSalary: Salary = jobList.maxSalary()
+      val salaryGap = maxSalary.value - job.salary.value
+      return withError({ NegativeSalary }) {
+        converter.convertUsdToEurRaisingNegativeAmount(salaryGap)
+      }
+    }
+
+    // if we want to accumulate errors inside a dedicated data structure
+    // if it fails, we would get at least one error, so we can use the NonEmptyList<A> or Nel<A>
+    // we can use mapOrAccumulate to accumulate errors
+    // inside, it uses a dedicated RaiseAccumulate<A> context, which overrides the raise method
+    // and creates a NonEmptyList<E> containing only the risen logic typed error
+    context(Raise<NonEmptyList<JobError>>)
+    fun getSalaryGapWithMax(jobIdList: List<JobId>): List<Double> =
+      mapOrAccumulate(jobIdList) { getSalaryGapWithMax(it) }
+
+    // what if we want directly to use the RaiseAccumulate<A> context?
+    // now we can use directly the extension function over the Iterable<A> type
+    context(RaiseAccumulate<JobError>)
+    fun getSalaryGapWithMax(jobIdList: List<JobId>): List<Double> =
+      jobIdList.mapOrAccumulate { getSalaryGapWithMax(it) }
+
+    // we could even get an Either<Nel<E>, List<B>> instead or Raise<E> context
+    fun getSalaryGapWithMaxEither(
+      jobIdList: List<JobId>
+    ): Either<NonEmptyList<JobError>, List<Double>> =
+      jobIdList.mapOrAccumulate { getSalaryGapWithMax(it) }
+
+    // collect errors and combine them into a custom type
+    context(Raise<JobErrors>)
+    fun getSalaryGapWithMaxJobErrors(jobIdList: List<JobId>) =
+      mapOrAccumulate(jobIdList, ::combine) {
+        withError({ jobError -> JobErrors(jobError.toString()) }) { getSalaryGapWithMax(it) }
+      }
+
+    // combining errors into a monoid
+    private fun combine(one: JobErrors, other: JobErrors): JobErrors =
+      JobErrors("${one.messages}, ${other.messages}")
   }
+
+  sealed interface CurrencyConversionError
+
+  data object NegativeAmount : CurrencyConversionError
 
   // would bubble up, and not converted to raise
   class CurrencyConverter {
@@ -123,6 +221,13 @@ object Raises {
         // we wrap here the throwable to our domain model and raise our domain error
         println("Oh, some error happened in the background: $exception")
         raise(NegativeSalary)
+      }
+
+    // we introduce new type of error for currencies, with new raise context
+    context(Raise<NegativeAmount>)
+    fun convertUsdToEurRaisingNegativeAmount(amount: Double?): Double =
+      catch({ currencyConverter.convertUsdToEur(amount) }) { _: IllegalArgumentException ->
+        raise(NegativeAmount)
       }
   }
 
@@ -152,13 +257,14 @@ object Raises {
     // won't work if we don't provide a Raise context
     // val myJobs = jobs.findById(appleJobId)
 
+    val converter = RaiseCurrencyConverter(CurrencyConverter())
+
     // using fold for raise
-    val jobService = JobsService(jobs)
+    val jobService = JobsService(jobs, converter)
     jobService.printSalaryFold(appleJobId)
     // Job salary for job with id 1 is Salary(value=100000.0)
 
-    // another fold example
-    val converter = RaiseCurrencyConverter(CurrencyConverter())
+    // another fold examples
     val salaryService = SalaryService(converter)
     salaryService.printSalaryConvertedFold(-100.0)
     // An exception was thrown: java.lang.IllegalArgumentException: Amount must be positive
@@ -169,5 +275,54 @@ object Raises {
     val fallbackAmount = recover({ converter.convertUsdToEur2(-1.0) }) { _: JobError -> 0.0 }
     println("The fallback amount is $fallbackAmount")
     // The fallback amount is 0.0
+
+    // composition
+    fold(
+      { jobService.getSalaryGapWithMax(JobId(1)) },
+      { error -> println("An error was raised: $error") },
+      { salaryGap -> println("The salary gap is $salaryGap") }
+    )
+
+    // same but with errors
+    fold(
+      { jobService.getSalaryGapWithMax(JobId(42)) },
+      { error -> println("An error was raised: $error") },
+      { salaryGap -> println("The salary gap is $salaryGap") }
+    )
+
+    // composition with logic errors from different hierarchies
+    fold(
+      { jobService.getSalaryGapWithMaxInEur(JobId(1)) },
+      { error -> println("An error was raised: $error") },
+      { salaryGap -> println("The salary gap in EUR is $salaryGap") }
+    )
+
+    // same but with the errors
+    fold(
+      { jobService.getSalaryGapWithMaxInEur(JobId(42)) },
+      { error -> println("An error was raised: $error") },
+      { salaryGap -> println("The salary gap in EUR is $salaryGap") }
+    )
+
+    // accumulating errors
+    fold(
+      { jobService.getSalaryGapWithMax(listOf(JobId(1), JobId(2))) },
+      { error -> println("The risen errors are: $error") },
+      { salaryGap -> println("The list of salary gaps is $salaryGap") }
+    )
+
+    // same but with the errors
+    fold(
+      { jobService.getSalaryGapWithMax(listOf(JobId(1), JobId(42), JobId(-1))) },
+      { error -> println("The risen errors are: $error") },
+      { salaryGap -> println("The list of salary gaps is $salaryGap") }
+    )
+
+    // combining errors into a custom type
+    fold(
+      { jobService.getSalaryGapWithMaxJobErrors(listOf(JobId(-1), JobId(42))) },
+      { error -> println("The risen errors are: $error") },
+      { salaryGaps -> println("The salary gaps are $salaryGaps") }
+    )
   }
 }
