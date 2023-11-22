19fa362900300545dcbc0378c14fdc89c24bc730
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2023-11-22 20:32:38 +0100

    feat: add atomicity examples using Arrow Atomic

---
+++ kotlin/code/_00_playground/src/main/kotlin/threads/Atomicity.kt
@@ -0,0 +1,68 @@
+@file:Suppress("MagicNumber")
+
+package threads
+
+import arrow.atomic.AtomicBoolean
+import arrow.atomic.AtomicInt
+import arrow.atomic.update
+import arrow.atomic.value
+import arrow.fx.coroutines.parMap
+import kotlinx.coroutines.runBlocking
+
+// 8. Multiplatform atomicity with Arrow Atomic
+
+object Atomicity {
+  data class User(val id: UserId, val name: UserName, var age: UserAge, val status: UserStatus)
+
+  data class UserStatus(val loggedIn: LoggedIn)
+
+  @JvmInline value class LoggedIn(val atomic: AtomicBoolean)
+
+  @JvmInline value class UserId(val value: String)
+
+  @JvmInline value class UserName(val value: String)
+
+  @JvmInline value class UserAge(val atomic: AtomicInt)
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    runBlocking {
+      val user =
+        User(
+          UserId("1"),
+          UserName("John Doe"),
+          UserAge(AtomicInt(30)),
+          UserStatus(LoggedIn(AtomicBoolean(false)))
+        )
+
+      // extension value access
+      println("Initial age: ${user.age.atomic.value}")
+      println("Initial login status: ${user.status.loggedIn.atomic.value}")
+
+      // Update age atomically
+      user.age.atomic.update { currentAge -> currentAge + 1 }
+
+      // Update login status atomically
+      user.status.loggedIn.atomic.update { currentStatus -> !currentStatus }
+
+      println("Updated age: ${user.age.atomic.value}")
+      println("Updated login status: ${user.status.loggedIn.atomic.value}")
+
+      println("------------------------------")
+
+      // Parallel updates
+      val otherUser =
+        User(
+          UserId("1"),
+          UserName("John Doe"),
+          UserAge(AtomicInt(30)),
+          UserStatus(LoggedIn(AtomicBoolean(false)))
+        )
+
+      (0 ..< 1000).parMap {
+        otherUser.status.loggedIn.atomic.update { currentStatus -> !currentStatus }
+      }
+      println("Final login status: ${otherUser.status.loggedIn.atomic.value}") // false
+    }
+  }
+}
