b61861e8884b1d45d9779e4065c4db78f7e5bb30
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2023-12-17 21:07:57 +0100

    refactor: change naming mostly on spring examples

--- kotlin/code/spring/functional/src/main/kotlin/com/functional/domain/PersonHandler.kt
+++ kotlin/code/spring/functional/src/main/kotlin/com/functional/domain/PersonHandler.kt
@@ -1,32 +1,37 @@
 package com.functional.domain
 
 import arrow.core.raise.Raise
+import arrow.core.raise.catch
 import com.functional.infrastructure.PersonDTO
 import com.functional.infrastructure.PersonRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 interface PersonHandler {
-    context(Raise<Error.GenericError>)
+    context(Raise<Error.Internal>)
     suspend fun readAll(): Flow<Person>
 
     context(Raise<Error>)
     suspend fun readOne(id: Long): Person
 
     sealed class Error(val message: String) {
-        data class PersonNotFound(val id: Long) : Error("Person with id $id not found")
-        data object GenericError : Error("Something went wrong")
+        data class NotFound(val id: Long) : Error("Person with id $id not found")
+        data object Internal : Error("Something went wrong")
     }
 
     companion object {
         operator fun invoke(personRepository: PersonRepository): PersonHandler = object : PersonHandler {
-            context(Raise<Error.GenericError>)
+            context(Raise<Error.Internal>)
             override suspend fun readAll(): Flow<Person> =
-                personRepository.findAll().map(::toDomain)
+                catch({ personRepository.findAll().map(::toDomain) }) {
+                    raise(Error.Internal)
+                }
 
             context(Raise<Error>)
             override suspend fun readOne(id: Long): Person =
-                personRepository.findById(id)?.let(::toDomain) ?: raise(Error.PersonNotFound(id))
+                catch({ personRepository.findById(id)?.let(::toDomain) ?: raise(Error.NotFound(id)) }) {
+                    raise(Error.Internal)
+                }
 
             private fun toDomain(person: PersonDTO): Person =
                 Person(
