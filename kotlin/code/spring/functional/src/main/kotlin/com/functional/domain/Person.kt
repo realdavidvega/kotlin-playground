36f2a3e557264f2dbf23a66c8f9a9eab503c4c25
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2024-01-09 23:08:40 +0100

    feat: Person value objects

--- kotlin/code/spring/functional/src/main/kotlin/com/functional/domain/Person.kt
+++ kotlin/code/spring/functional/src/main/kotlin/com/functional/domain/Person.kt
@@ -5,8 +5,31 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Person(
-  val id: Long,
-  val firstName: String,
-  val lastName: String,
-  val birthdate: LocalDate?
-)
+  val id: Id,
+  val firstName: FirstName,
+  val lastName: LastName,
+  val birthdate: BirthDate
+) {
+  @JvmInline @Serializable value class Id(val value: Long)
+
+  @JvmInline @Serializable value class FirstName(val value: String)
+
+  @JvmInline @Serializable value class LastName(val value: String)
+
+  @JvmInline @Serializable value class BirthDate(val value: LocalDate?)
+
+  companion object {
+    operator fun invoke(
+      id: Long,
+      firstName: String,
+      lastName: String,
+      birthdate: LocalDate?
+    ): Person =
+      Person(
+        id = Id(id),
+        firstName = FirstName(firstName),
+        lastName = LastName(lastName),
+        birthdate = BirthDate(birthdate)
+      )
+  }
+}
