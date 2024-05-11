1348da34b89ff2be1a5a266ee4903dab13ec883b
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2024-05-11 17:36:59 +0200

    feat: flows error handling

--- kotlin/code/playground/src/main/kotlin/flows/Flows.kt
+++ kotlin/code/playground/src/main/kotlin/flows/Flows.kt
@@ -1,20 +1,5 @@
 package flows
 
-import flows.Flows.andrewGarfield
-import flows.Flows.benAffleck
-import flows.Flows.chrisEvans
-import flows.Flows.chrisHemsworth
-import flows.Flows.ezraMiller
-import flows.Flows.galGadot
-import flows.Flows.henryCavill
-import flows.Flows.jasonMomoa
-import flows.Flows.jeremyRenner
-import flows.Flows.markRuffalo
-import flows.Flows.rayFisher
-import flows.Flows.robertDowneyJr
-import flows.Flows.scarlettJohansson
-import flows.Flows.tobeyMaguire
-import flows.Flows.tomHolland
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
@@ -24,8 +9,9 @@ import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.runBlocking
 
-// 8. Flows and Coroutines
+// 8. Flows and Coroutines - Part. I - Introduction
 
 object Flows {
   @JvmInline value class Id(val id: Int)
@@ -56,133 +42,158 @@ object Flows {
   val tomHolland: Actor = Actor(Id(12), FirstName("Tom"), LastName("Holland"))
   val tobeyMaguire: Actor = Actor(Id(13), FirstName("Tobey"), LastName("Maguire"))
   val andrewGarfield: Actor = Actor(Id(14), FirstName("Andrew"), LastName("Garfield"))
-}
 
-suspend fun main() {
-  // A Flow<T> is a reactive data structure that emits a sequence of type T values
+  @JvmStatic
+  fun main(args: Array<String>) {
+    runBlocking {
 
-  // We can create a flow from a finite list of values using the flowOf function
-  // Actually is not a data structure, but a concurrent data structure
-  val zackSnyderJusticeLeague: Flow<Flows.Actor> =
-    flowOf(henryCavill, galGadot, ezraMiller, rayFisher, benAffleck, jasonMomoa)
+      // A Flow<T> is a reactive data structure that emits a sequence of type T values
 
-  // Or create a flow from a list, a set, and so on using the asFlow extension function
-  // Again, we are emitting values
-  val avengers: Flow<Flows.Actor> =
-    listOf(robertDowneyJr, chrisEvans, markRuffalo, chrisHemsworth, scarlettJohansson, jeremyRenner)
-      .asFlow()
+      // We can create a flow from a finite list of values using the flowOf function
+      // Actually is not a data structure, but a concurrent data structure
+      val zackSnyderJusticeLeague: Flow<Actor> =
+        flowOf(henryCavill, galGadot, ezraMiller, rayFisher, benAffleck, jasonMomoa)
 
-  // If we have a function that returns a value, we can create a flow from it using the
-  // asFlow function as well
-  val theMostRecentSpiderManFun: () -> Flows.Actor = { tomHolland }
+      // Or create a flow from a list, a set, and so on using the asFlow extension function
+      // Again, we are emitting values
+      val avengers: Flow<Actor> =
+        listOf(
+            robertDowneyJr,
+            chrisEvans,
+            markRuffalo,
+            chrisHemsworth,
+            scarlettJohansson,
+            jeremyRenner,
+          )
+          .asFlow()
 
-  // Emits the value returned by the function
-  val theMostRecentSpiderMan: Flow<Flows.Actor> = theMostRecentSpiderManFun.asFlow()
+      // If we have a function that returns a value, we can create a flow from it using the
+      // asFlow function as well
+      val theMostRecentSpiderManFun: () -> Actor = { tomHolland }
 
-  // Flows are consumed, and will emit values at the time
+      // Emits the value returned by the function
+      val theMostRecentSpiderMan: Flow<Actor> = theMostRecentSpiderManFun.asFlow()
 
-  // The flow function is the most general way to create a flow, and emit the values
-  // All the above Flow constructors are in fact built with the flow function
-  val spiderMen: Flow<Flows.Actor> = flow {
-    emit(tobeyMaguire)
-    emit(andrewGarfield)
-    emit(tomHolland)
-  }
+      // Flows are consumed, and will emit values at the time
 
-  // The lambda passed as an input parameter to the flow function defines an instance
-  // of a functional interface called FlowCollector as its receiver. And gives some
-  // functions like emit
+      // The flow function is the most general way to create a flow, and emit the values
+      // All the above Flow constructors are in fact built with the flow function
+      val spiderMen: Flow<Actor> = flow {
+        emit(tobeyMaguire)
+        emit(andrewGarfield)
+        emit(tomHolland)
+      }
 
-  // It’s easy to define the previous factory methods in terms of the flow function
-  fun <T> flowOf(vararg values: T): Flow<T> = flow {
-    for (value in values) {
-      emit(value)
-    }
-  }
+      // The lambda passed as an input parameter to the flow function defines an instance
+      // of a functional interface called FlowCollector as its receiver. And gives some
+      // functions like emit
 
-  // Needs to be called from a coroutine as it's a suspend function
-  spiderMen.collect { println(it) }
+      // It’s easy to define the previous factory methods in terms of the flow function
+      fun <T> flowOf(vararg values: T): Flow<T> = flow {
+        for (value in values) {
+          emit(value)
+        }
+      }
 
-  // The collect method is a suspending function since it has to wait and suspend for consuming
-  // the emitted values without blocking a thread.
-  // suspend fun <T> Flow<T>.collect(collector: FlowCollector<T>): Unit = TODO()
+      // Needs to be called from a coroutine as it's a suspend function
+      spiderMen.collect { println(it) }
 
-  // These emit are not necessarily immediate or synchronous, they can take a while
-  val slowerSpiderMen: Flow<Flows.Actor> = flow {
-    emit(tobeyMaguire)
-    delay(1000)
-    emit(andrewGarfield)
-    delay(1000)
-    emit(tomHolland)
-  }
+      println("-------------------")
 
-  // Since it’s a reactive data structure, the values in a flow are only computed once requested
-  // A Flow<T> is just a definition of calculating the values, not the values themselves,
-  // which is a fundamental difference with collections, sequences, and iterables
+      // The collect method is a suspending function since it has to wait and suspend for consuming
+      // the emitted values without blocking a thread.
+      // suspend fun <T> Flow<T>.collect(collector: FlowCollector<T>): Unit = TODO()
 
-  // We can listen when the item is created or when the flow start emitting things
-  // And do some stuff like this:
-  slowerSpiderMen
-    .onStart { println("started") }
-    .onEach { delay(100) }
-    .onCompletion { println("flow finished") }
-    .collect { println(it) }
+      // These emit are not necessarily immediate or synchronous, they can take a while
+      val slowerSpiderMen: Flow<Actor> = flow {
+        emit(tobeyMaguire)
+        delay(1000)
+        emit(andrewGarfield)
+        delay(1000)
+        emit(tomHolland)
+      }
 
-  // The onStart function lets us add operations to be executed when the flow is started.
-  // fun <T> Flow<T>.onStart(action: suspend FlowCollector<T>.() -> Unit): Flow<T> = TODO()
+      // Since it’s a reactive data structure, the values in a flow are only computed once requested
+      // A Flow<T> is just a definition of calculating the values, not the values themselves,
+      // which is a fundamental difference with collections, sequences, and iterables
 
-  // When does a flow start to emit values? A flow is started when a terminal operation is
-  // called on it. So far, we’ve seen the collect function as a terminal operation.
-  val spiderMenWithLatency: Flow<Flows.Actor> = flow {
-    delay(1000)
-    emit(tobeyMaguire)
-    emit(andrewGarfield)
-    emit(tomHolland)
-  }
+      // We can listen when the item is created or when the flow start emitting things
+      // And do some stuff like this:
+      slowerSpiderMen
+        .onStart { println("started") }
+        .onEach { delay(100) }
+        .onCompletion { println("flow finished") }
+        .collect { println(it) }
 
-  //  The lambda of the onStart function is executed immediately after the terminal operation.
-  //  It does not wait for the first element to be emitted
-  spiderMenWithLatency
-    .onStart { println("Starting Spider-Men flow") }
-    .collect { println(it) }
+      println("-------------------")
 
-  // The exciting thing is that the action lambda has a FlowCollector<T> as the receiver,
-  // meaning we can emit values inside it
-  spiderMenWithLatency
-    .onStart { emit(Flows.Actor(Flows.Id(15), Flows.FirstName("Paul"), Flows.LastName("Soles"))) }
-    .collect { println(it) }
+      // The onStart function lets us add operations to be executed when the flow is started.
+      // fun <T> Flow<T>.onStart(action: suspend FlowCollector<T>.() -> Unit): Flow<T> = TODO()
 
-  // The onEach function is used to apply a lambda to each value emitted by the flow
-  spiderMen
-    .onEach { delay(1000) }
-    .collect { println(it) }
+      // When does a flow start to emit values? A flow is started when a terminal operation is
+      // called on it. So far, we’ve seen the collect function as a terminal operation.
+      val spiderMenWithLatency: Flow<Actor> = flow {
+        delay(1000)
+        emit(tobeyMaguire)
+        emit(andrewGarfield)
+        emit(tomHolland)
+      }
 
-  // We can even use the onEach function as a surrogate of the collect function.
-  // We can pass to the lambda we would have passed to the collect function to the onEach function.
-  // At this point, calling collect will trigger the effective execution of the flow.
-  spiderMen.onEach {
-    delay(1000)
-    println(it)
-  }.collect()
+      //  The lambda of the onStart function is executed immediately after the terminal operation.
+      //  It does not wait for the first element to be emitted
+      spiderMenWithLatency.onStart { println("Starting Spider-Men flow") }.collect { println(it) }
 
-  // We can use the onCompletion function to add a lambda to be executed when the flow is completed
-  spiderMen
-    .onEach {
-      println(it)
-    }
-    .onCompletion { println("End of the Spider Men flow") }
-    .collect()
+      println("-------------------")
 
-  // Infinite flow, does not emit values during creation
-  val infiniteJLFlowActors: Flow<Flows.Actor> = flow {
-    // It won't block the application, they run in their separate coroutines
-    while (true) {
-      emit(henryCavill)
-      emit(galGadot)
-      emit(ezraMiller)
-      emit(rayFisher)
-      emit(benAffleck)
-      emit(jasonMomoa)
+      // The exciting thing is that the action lambda has a FlowCollector<T> as the receiver,
+      // meaning we can emit values inside it
+      spiderMenWithLatency
+        .onStart {
+          emit(Actor(Id(15), FirstName("Paul"), LastName("Soles")))
+        }
+        .collect { println(it) }
+
+      println("-------------------")
+
+      // The onEach function is used to apply a lambda to each value emitted by the flow
+      spiderMen.onEach { delay(1000) }.collect { println(it) }
+
+      println("-------------------")
+
+      // We can even use the onEach function as a surrogate of the collect function.
+      // We can pass to the lambda we would have passed to the collect function to the onEach
+      // function.
+      // At this point, calling collect will trigger the effective execution of the flow.
+      spiderMen
+        .onEach {
+          delay(1000)
+          println(it)
+        }
+        .collect()
+
+      println("-------------------")
+
+      // We can use the onCompletion function to add a lambda to be executed when the flow is
+      // completed
+      spiderMen
+        .onEach { println(it) }
+        .onCompletion { println("End of the Spider Men flow") }
+        .collect()
+
+      println("-------------------")
+
+      // Infinite flow, does not emit values during creation
+      val infiniteJLFlowActors: Flow<Actor> = flow {
+        // It won't block the application, they run in their separate coroutines
+        while (true) {
+          emit(henryCavill)
+          emit(galGadot)
+          emit(ezraMiller)
+          emit(rayFisher)
+          emit(benAffleck)
+          emit(jasonMomoa)
+        }
+      }
     }
   }
 }
