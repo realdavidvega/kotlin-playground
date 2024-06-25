f4eea16aca674fbd57ff3c5e5655526bd776a709
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2024-06-25 12:28:15 +0200

    feat: example of testcontainers with ollama

---
+++ kotlin/code/playground/src/test/kotlin/ai/local/OllamaSpec.kt
@@ -0,0 +1,53 @@
+package ai.local
+
+import com.github.dockerjava.api.model.Image
+import dev.langchain4j.model.ollama.OllamaChatModel
+import io.kotest.core.spec.style.StringSpec
+import org.testcontainers.DockerClientFactory
+import org.testcontainers.ollama.OllamaContainer
+import org.testcontainers.utility.DockerImageName
+
+class OllamaSpecScope(private val model: OllamaChatModel) {
+  fun String.generates(block: (String) -> String?) {
+    block(model.generate(this))
+  }
+}
+
+fun createOrUseOllamaImage(
+  modelName: String
+): OllamaContainer {
+  val dockerImageName = modelName.split(":").let { (prefix, version) ->
+    "tc-ollama-gemma-$prefix-$version"
+  }
+
+  val listImagesCmd: List<Image> = DockerClientFactory.lazyClient()
+    .listImagesCmd()
+    .withImageNameFilter(dockerImageName)
+    .exec()
+
+  return if (listImagesCmd.isEmpty()) {
+    println("Creating a new Ollama container with $modelName image...")
+    val ollama = OllamaContainer("ollama/ollama:0.1.26")
+    ollama.start()
+    ollama.execInContainer("ollama", "pull", modelName)
+    ollama.commitToImage(dockerImageName)
+    ollama
+  } else {
+    println("Using existing Ollama container with $modelName image...")
+    val ollama = OllamaContainer(
+      DockerImageName.parse(dockerImageName)
+        .asCompatibleSubstituteFor("ollama/ollama")
+    )
+    ollama.start()
+    ollama
+  }
+}
+
+fun ollamaTest(modelName: String, body: context (OllamaSpecScope, StringSpec) () -> Unit): StringSpec.() -> Unit = {
+  val container = createOrUseOllamaImage(modelName)
+  val model: OllamaChatModel = OllamaChatModel.builder()
+    .baseUrl(String.format("http://%s:%d", container.host, container.firstMappedPort))
+    .modelName(modelName)
+    .build()
+  body(OllamaSpecScope(model), this)
+}
