462d52f2cc12834c70201df6dcc8031a4a81b65f
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2024-12-17 21:12:58 +0100

    feat: managing threads

--- kotlin/learning/03_kotlin_coroutines_concurrency/build.gradle.kts
+++ kotlin/learning/03_kotlin_coroutines_concurrency/build.gradle.kts
@@ -1,26 +1,27 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
+  kotlin("jvm") version "2.0.0"
+  id("com.diffplug.spotless") version "6.19.0"
 }
 
-group = "org.example"
+group = "com.rockthejvm"
+
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
-    implementation("ch.qos.logback:logback-classic:1.5.6")
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
-    testImplementation("junit:junit:4.13.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+  implementation("ch.qos.logback:logback-classic:1.5.6")
+  testImplementation("org.jetbrains.kotlin:kotlin-test")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+  testImplementation("junit:junit:4.13.2")
 }
 
-tasks.test {
-    useJUnitPlatform()
+spotless {
+  kotlin { ktfmt().googleStyle() }
+  kotlinGradle { ktfmt().googleStyle() }
 }
 
-kotlin {
-    jvmToolchain(21)
-}
+tasks.test { useJUnitPlatform() }
+
+kotlin { jvmToolchain(21) }
