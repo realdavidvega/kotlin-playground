4191356bf7cd7fbb98b7de9aa879a4d2f5225f6d
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2025-01-27 11:38:38 +0100

    feat(lang): add boolean builder

--- kotlin/code/playground/modules/lang/src/main/kotlin/extensions/Kotlin.kt
+++ kotlin/code/playground/modules/lang/src/main/kotlin/extensions/Kotlin.kt
@@ -1,6 +1,8 @@
 package extensions
 
 import java.io.Serializable
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 /**
  * Handful extensions for `kotlin`.
@@ -74,4 +76,60 @@ object Kotlin {
 
   /** Converts this quadruple into a list. */
   fun <T> Quintuple<T, T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth, fifth)
+
+  /** Holds a boolean value and allows local modification. */
+  class BooleanHolder(var value: Boolean) {
+    fun setTrue() {
+      value = true
+    }
+
+    fun setFalse() {
+      value = false
+    }
+
+    fun toggle() {
+      value = !value
+    }
+  }
+
+  /** Builds a boolean value from a lambda, allowing local modification, and returns the value. */
+  inline fun buildBoolean(
+    initialValue: Boolean = false,
+    builderAction: BooleanHolder.() -> Unit,
+  ): Boolean {
+    val holder = BooleanHolder(initialValue)
+    builderAction(holder)
+    return holder.value
+  }
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    runBlocking {
+      val quadruple = Quadruple(1, 2, 3, 4)
+      println(quadruple)
+
+      val quintuple = Quintuple(1, 2, 3, 4, 5)
+      println(quintuple)
+
+      // example program that uses buildBoolean while loop is true and then exits it when false
+      var workingHours = 0
+      while (true) {
+        val condition = buildBoolean {
+          if (workingHours == 9) {
+            println("I've reached my working hours limit today, wrapping up!")
+            delay(500L)
+            setTrue()
+          }
+        }
+        if (condition) {
+          println("I'm out, goodbye!")
+          break
+        }
+
+        println("I'm working... don't disturb me! I've worked $workingHours hours so far")
+        delay(1000L)
+        workingHours++
+      }
+    }
+  }
 }
